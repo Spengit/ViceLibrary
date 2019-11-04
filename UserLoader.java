1ccacb00ce99704bfc8cfdb4256fc725b2e1816f
Spengit <es19@email.sc.edu>
2019-11-03 20:10:27 -0500

    Updates to the user loader

--- UserLoader.java
+++ UserLoader.java
@@ -23,8 +23,19 @@ public class UserLoader {
 				String type = (String)personJSON.get("type");
 				String fines = (String) personJSON.get("fines").toString();
 				
-				Users.addUser(new PersonalUser(firstName, lastName, 
-						phoneNumber, address,email, Double.valueOf(fines)));
+				switch(type) {
+				case  "P" :
+					Users.addUser(new PersonalUser(firstName, lastName, 
+							phoneNumber, address,email, Double.valueOf(fines)));
+					break;
+				case "L" :
+					Users.addUser(new Librarian(firstName, lastName, 
+							phoneNumber, address,email, Double.valueOf(fines)));
+					break;
+				case "T" :
+					Users.addUser(new Teacher(firstName, lastName, 
+							phoneNumber, address,email, Double.valueOf(fines)));
+				}
 				}
 		} catch (Exception e){
 			e.printStackTrace();
